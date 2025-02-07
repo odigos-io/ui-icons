5e446a0b7a9203d6428272c823c4b2fcbed64dd5
Ben Elferink <ben.elferink@icloud.com>
2025-02-07 18:32:03 +0200

    fix: build error

--- src/icons/actions/pii-masking-icon/pii-masking-icon.stories.tsx
+++ src/icons/actions/pii-masking-icon/pii-masking-icon.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { PiiMaskingIcon } from '.'
-import { type SVG } from '../../../@types'
-import { type StoryFn } from '@storybook/react'
+import type { SVG } from '../../../@types'
+import type { StoryFn } from '@storybook/react'
 
 export default {
   title: 'Actions/PiiMaskingIcon',
--- src/icons/on-off/light-on-icon/light-on-icon.stories.tsx
+++ src/icons/on-off/light-on-icon/light-on-icon.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { LightOnIcon } from '.'
-import { type SVG } from '../../@types'
-import { type StoryFn } from '@storybook/react'
+import type { SVG } from '../../../@types'
+import type { StoryFn } from '@storybook/react'
 
 export default {
   title: 'Common/LightOnIcon',
