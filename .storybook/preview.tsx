c940fed5c88a4906252f4ab5bca7885a0992098d
Ben Elferink <ben.elferink@icloud.com>
2025-02-07 19:21:10 +0200

    fix: storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react'
-import { Theme } from '@odigos/ui-theme'
+import React, { useEffect } from 'react'
+import Theme from '@odigos/ui-theme'
 import type { Preview } from '@storybook/react'
 import { ToggleDarkMode } from '@odigos/ui-components'
+
+const DarkModeSettings = () => {
+  const { darkMode } = Theme.useDarkMode()
+
+  useEffect(() => {
+    document.body.style.backgroundColor = darkMode ? '#000' : '#fff'
+  }, [darkMode])
+
+  return null
+}
 
 const preview: Preview = {
   decorators: [
     // 👇 Defining the decorator in the preview file applies it to all stories
     (Story, {}) => {
-      const [darkMode, setDarkMode] = useState(true)
-
       return (
-        <Theme.Provider darkMode={darkMode}>
-          <ToggleDarkMode
-            darkMode={darkMode}
-            setDarkMode={(bool) => {
-              setDarkMode(bool)
-              document.body.style.backgroundColor = bool ? '#000' : '#fff'
-            }}
-          />
+        <Theme.Provider>
+          <ToggleDarkMode />
+          <DarkModeSettings />
 
           <div style={{ marginTop: '1rem' }}>
             <Story />
